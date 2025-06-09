32613858a78296529574cd14308428a4fae13196
ivanlele <ivanlele402@gmail.com>
2025-06-09 13:56:31 +0300

    Update .gitignore and enhance xcframework creation script for better framework management

--- scripts/create_xcframework.sh
+++ scripts/create_xcframework.sh
@@ -2,46 +2,83 @@
 
 set -e
 
-if [ -d "xcframeworks" ]; then
-    rm -rf xcframeworks
-fi
+XCFWNAME="ZkNeuralRustCore"
+FWNAME="ZkNeuralRustCoreLib"
 
-if [ -z "$CODE_SIGNER" ]; then
-    echo "CODE_SIGNER is not set. Please set it to your code signing identity. Use \"security find-identity -v -p codesigning\" to find your identity."
-    exit 1
-fi 
+function create_framework() {
+    for fw in "$@"; do
+        copy_framework_files "${fw}"
+    done
 
+    local fw_paths=()
+    for fw in "$@"; do
+        fw_paths+=("-framework" "Frameworks/${fw}/$FWNAME.framework")
+    done
+
+    for fw in "$@"; do
+        {
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+        echo "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"
+        echo "<plist version=\"1.0\">"
+        echo "<dict>"
+        echo "    <key>CFBundlePackageType</key>"
+        echo "    <string>FMWK</string>"
+        echo "    <key>CFBundleIdentifier</key>"
+        echo "    <string>Rarilabs.$FWNAME</string>"
+        echo "    <key>CFBundleExecutable</key>"
+        echo "    <string>$FWNAME</string>"
+        echo "    <key>CFBundleShortVersionString</key>"
+        echo "    <string>1.0.0</string>"
+        echo "    <key>CFBundleVersion</key>"
+        echo "    <string>3</string>"
+        echo "    <key>MinimumOSVersion</key>"
+        echo "    <string>100</string>"
+        echo "</dict>"
+        echo "</plist>"
+        } > "Frameworks/$fw/$FWNAME.framework/Info.plist"
+    done
+
+    rm -rf "Frameworks/$XCFWNAME.xcframework"
+    xcrun xcodebuild -create-xcframework \
+        "${fw_paths[@]}" \
+        -output "Frameworks/$XCFWNAME.xcframework"
+
+    
+    if [ -n "$CODE_SIGNER" ]; then
+        codesign --timestamp -s "$CODE_SIGNER" "Frameworks/$XCFWNAME.xcframework"
+    fi
+}
+
+function copy_framework_files() {
+    local FRAMEWORK_PATH="Frameworks/$1/ZkNeuralRustCoreLib.framework"
+
+    mkdir -p "$FRAMEWORK_PATH/Headers"
+
+    cp headers/zk_neural_rust_core.h "$FRAMEWORK_PATH/Headers/ZkNeuralRustCoreLib.h"
+
+    mkdir -p $FRAMEWORK_PATH/Modules
+    {
+    echo "framework module ZkNeuralRustCoreLib {"
+    echo "\tumbrella header \"ZkNeuralRustCoreLib.h\""
+    echo "\texport *"
+    echo "\tmodule * { export * }"
+    echo "}"
+    } > $FRAMEWORK_PATH/Modules/module.modulemap
+
+    cp target/$1/release/libzk_neural_rust_core.a $FRAMEWORK_PATH/ZkNeuralRustCoreLib
+}
+
+rm -rf Frameworks
 
 strip -x target/aarch64-apple-ios/release/libzk_neural_rust_core.a target/aarch64-apple-ios-sim/release/libzk_neural_rust_core.a
 
-xcodebuild -create-xcframework \
-    -library target/aarch64-apple-ios/release/libzk_neural_rust_core.a -headers headers \
-    -library target/aarch64-apple-ios-sim/release/libzk_neural_rust_core.a -headers headers \
-    -output xcframeworks/ZkNeuralRustCore.xcframework
+frameworks=("aarch64-apple-ios" "aarch64-apple-ios-sim")
+create_framework "${frameworks[@]}"
 
-mkdir -p xcframeworks/ZkNeuralRustCore.xcframework/ios-arm64/Modules
-mkdir -p xcframeworks/ZkNeuralRustCore.xcframework/ios-arm64-simulator/Modules
+pushd "Frameworks"
+zip -X -9 -r "$XCFWNAME.xcframework.zip" "$XCFWNAME.xcframework" -i */ZkNeuralRustCoreLib -i *.plist -i *.h -i *.modulemap
+popd
 
-{
-echo "framework module ZkNeuralRustCore {"
-echo "    umbrella header \"zk_neural_rust_core.h\""
-echo "    export *"
-echo "    module * { export * }"
-echo "}"
-} > xcframeworks/ZkNeuralRustCore.xcframework/ios-arm64/Modules/module.modulemap
+swift package compute-checksum "Frameworks/ZkNeuralRustCore.xcframework.zip" > Frameworks/ZkNeuralRustCore.xcframework.zip.checksum
 
-{
-echo "framework module ZkNeuralRustCore {"
-echo "    umbrella header \"zk_neural_rust_core.h\""
-echo "    export *"
-echo "    module * { export * }"
-echo "}"
-} > xcframeworks/ZkNeuralRustCore.xcframework/ios-arm64-simulator/Modules/module.modulemap
-
-codesign --timestamp -s "$CODE_SIGNER" xcframeworks/ZkNeuralRustCore.xcframework
-
-zip -9 -r "xcframeworks/ZkNeuralRustCore.xcframework.zip" "xcframeworks/ZkNeuralRustCore.xcframework"
-
-swift package compute-checksum "xcframeworks/ZkNeuralRustCore.xcframework.zip" > xcframeworks/ZkNeuralRustCore.xcframework.zip.checksum
-
-echo "ZkNeuralRustCore.xcframework.zip checksum: $(cat xcframeworks/ZkNeuralRustCore.xcframework.zip.checksum)"
+echo "ZkNeuralRustCore.xcframework.zip checksum: $(cat Frameworks/ZkNeuralRustCore.xcframework.zip.checksum)"
